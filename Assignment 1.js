10bc3a893693620ca482ce2afc31b30586de8c44
Dharchini <76682477+Dharchini@users.noreply.github.com>
2022-11-08 23:31:47 +0530

    Add files via upload

---
+++ Assignment 1.js
@@ -0,0 +1,30 @@
+//JAVASCRIPT ASSIGNMENT 1
+
+// decrease the rainfall by 10% to account for runoff
+
+reservoir_volume = 4.445e8;
+rainfall = 5e6;
+rainfall= rainfall - (rainfall/10);
+console.log("After decreasing 10% of rainfall as runoff :",rainfall);
+
+// add the rainfall to the reservoir_volume
+
+reservoir_volume = reservoir_volume + rainfall;
+console.log("\nUpdated reservoir volume : ",reservoir_volume);
+
+// increase reservoir_volume by 5% to account for stormwater that flows into the reservoir in the days following the storm
+
+reservoir_volume = reservoir_volume + (reservoir_volume*0.5);
+console.log("Increasing reservoir volume by 5% : ",reservoir_volume);
+
+// decrease reservoir_volume by 5% to account for evaporation
+
+reservoir_volume = reservoir_volume - (reservoir_volume*0.5);
+console.log("Decreasing reservoir volume by 5% : ",reservoir_volume);
+
+// subtract 2.5e5 cubic meters from reservoir_volume to account for water that's piped to arid regions
+
+reservoir_volume = reservoir_volume - 2.5e5;
+
+// print the new value of the reservoir_volume variable
+console.log("Updated reservoir volume : ",reservoir_volume);
